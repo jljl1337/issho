78a1999df1c1adf0de586b8834ac68bbebc6a631
jljl1337 <lckjack123@gmail.com>
2025-11-26 05:23:40 -0700

    feat: improve error handling in getVersion function

--- web/app/lib/db/version.ts
+++ web/app/lib/db/version.ts
@@ -1,11 +1,14 @@
+import { getError } from "~/lib/db/common";
 import { customFetch } from "~/lib/db/fetch";
 
 export async function getVersion() {
   const response = await customFetch("/api/version", "GET");
-  if (!response.ok) {
-    const error = await response.text();
+
+  const error = await getError(response);
+  if (error != null) {
     return { data: null, error };
   }
+
   const data: { version: string } = await response.json();
   return { data: data.version, error: null };
 }
