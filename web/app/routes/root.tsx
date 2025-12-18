b8a8c8c5ccc7d4683b82ce043594de75f57cd937
jljl1337 <lckjack123@gmail.com>
2025-12-18 10:30:44 -0700

    feat: implement routing logic in root page

--- web/app/routes/root.tsx
+++ web/app/routes/root.tsx
@@ -1,5 +1,33 @@
-import { redirect } from "react-router";
+import { useEffect } from "react";
+import { useNavigate } from "react-router";
 
-export async function clientLoader() {
-  return redirect("/home");
+import { Spinner } from "~/components/ui/spinner";
+
+import { CenteredPage } from "~/components/layouts/centered-page";
+import { useSession } from "~/contexts/session-context";
+import { isUser } from "~/lib/validation/role";
+
+export default function Page() {
+  const { isLoggedIn, isLoading, user } = useSession();
+  const navigate = useNavigate();
+
+  // Redirect based on auth state
+  useEffect(() => {
+    if (!isLoading) {
+      if (isLoggedIn && user) {
+        const destination = isUser(user.role) ? "/home" : "/admin/posts";
+        navigate(destination);
+      } else {
+        navigate("/auth/sign-in");
+      }
+    }
+  }, [isLoggedIn, isLoading, user, navigate]);
+
+  return (
+    <div className="h-svh">
+      <CenteredPage>
+        <Spinner className="size-8" />
+      </CenteredPage>
+    </div>
+  );
 }
